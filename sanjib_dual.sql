b61918ac739658a8a5683c9af31db53d330915f4
Sanjib Panda <sanjib.panda@outlook.com>
2020-07-27 23:45:43 +0530

    Added new line select 6 from dual -- feature-02

--- sanjib_dual.sql
+++ sanjib_dual.sql
@@ -3,3 +3,5 @@ select 2 from dual;
 select 3 from dual; /* did changes from local dir */
 select 4 from dual; /* did chnages locally */
 select 5 from dual; /* chnages in branch feature-01 */
+select 6 from dual; /* chnages in branch feature-02 */
+
